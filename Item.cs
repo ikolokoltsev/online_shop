205d68642f22819e707c00629970387df20e10ec
Ivan Kolokoltsev <ikolokoltsev@gmail.com>
2025-10-07 21:15:38 +0200

    The price has been removed because this system does not use money, but barter

--- Item.cs
+++ Item.cs
@@ -4,13 +4,12 @@ class Item
 {
     public string Name;
     public string? Description;
-    public double Price;
     public string BelongsTo;
-    public Item(string name, string? description, double price, string belongsTo)
+
+    public Item(string name, string? description, string belongsTo)
     {
         Name = name;
         Description = description;
-        Price = price;
         BelongsTo = belongsTo;
     }
 }
